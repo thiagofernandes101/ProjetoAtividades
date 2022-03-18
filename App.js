689dbcfc7249fd70ecbef43af69ae163c73466a5
thiagofernandes101 <thiagofernandes101@outlook.com>
2022-03-17 23:21:42 -0300

    [FEAT] Criação do projeto

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './Telas/Home';
+import AtividadesScreen from './Telas/Atividades';
+import TipoAtividadeScreen from './Telas/TiposAtividades';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Bem vindo'}}></Stack.Screen>
+        <Stack.Screen name="Atividades" component={AtividadesScreen} options={{title: 'Atividades'}}></Stack.Screen>
+        <Stack.Screen name="TiposAtividades" component={TipoAtividadeScreen} options={{title: 'Tipos de Atividades'}}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
